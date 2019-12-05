13ce19449dd6244f00483bdd3ed1f1233d835360
Thalles Koester <thallesdella@gmail.com>
2019-12-04 22:51:02 -0300

    Code reformatted flags.c

--- include/flags.h
+++ include/flags.h
@@ -5,25 +5,27 @@
 #ifndef LOCALIZA_FLAGS_H
 #define LOCALIZA_FLAGS_H
 
-#include "structs.h"
 #include "dstring.h"
+#include "structs.h"
 
-#define FLAG_HELP 0
-#define FLAG_CASE 1
-#define FLAG_COUNT 2
-#define FLAG_NUMB 3
-#define FLAG_OUT 4
-#define FLAG_DEBUG 5
+//#define FLAG_DEBUG 0
+#define FLAG_HELP 1
+#define FLAG_CASE 2
+#define FLAG_COUNT 3
+#define FLAG_NUMB 4
+#define FLAG_OUT 5
 
 typedef int (*VecFlagsFunc)(dString);
 
-void initFlags(Flags *structFlags, Option *arrStructOption, VecFlagsFunc *func, int flagsCount);
+void initFlags(Flags *structFlags, Option *arrStructOption, VecFlagsFunc *func,
+               int flagsCount);
 
 void displayFlagHelp(dString scriptName, int exitCode);
 
 void displayFlagCount(Targets target);
 
-void checkFlagsExistence(Flags *flags, Option *option, int argc, dStringVector argv);
+void checkFlagsExistence(Flags *flags, Option *option, int argc,
+                         dStringVector argv);
 
 int getFlagStatus(Flags flags, int id);
 
@@ -39,4 +41,4 @@ int flagLineNumber(dString str);
 
 int flagOutput(dString str);
 
-#endif //LOCALIZA_FLAGS_H
+#endif // LOCALIZA_FLAGS_H
