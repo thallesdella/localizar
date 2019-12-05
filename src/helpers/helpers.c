4e44c837ad07700b5b74aa10691d22e823cbe8fa
Thalles Koester <thallesdella@gmail.com>
2019-12-04 23:58:18 -0300

    Add doc to helpers

--- src/helpers/helpers.c
+++ src/helpers/helpers.c
@@ -10,6 +10,16 @@
 #include <sys/stat.h>
 #include <time.h>
 
+/**
+ * Function: newLinePosition
+ * ----------------------------
+ *   @brief find the next end of the line.
+ *
+ *   @param stream file stream.
+ *   @param start position of file's start.
+ *
+ *   @return new line char position.
+ */
 int newLinePosition(FILE *stream, long int start) {
   int c = 0, i = 0;
   while (c != '\n' && !feof(stream)) {
@@ -20,6 +30,15 @@ int newLinePosition(FILE *stream, long int start) {
   return i;
 }
 
+/**
+ * Function: fileExists
+ * ----------------------------
+ *   @brief Verify if file exists.
+ *
+ *   @param path path to file.
+ *
+ *   @return 1 for true and 0 for false.
+ */
 int fileExists(dString path) {
   Stat statBuf;
   if (stat(path, &statBuf) == 0) {
@@ -28,6 +47,15 @@ int fileExists(dString path) {
   return 0;
 }
 
+/**
+ * Function: isFile
+ * ----------------------------
+ *   @brief verify if is file.
+ *
+ *   @param path path to file.
+ *
+ *   @return 1 for true and 0 for false.
+ */
 int isFile(dString path) {
   Stat statBuf;
   stat(path, &statBuf);
@@ -38,6 +66,15 @@ int isFile(dString path) {
   return 0;
 }
 
+/**
+ * Function: isDir
+ * ----------------------------
+ *   @brief verify if is directory.
+ *
+ *   @param path path to file.
+ *
+ *   @return 1 for true and 0 for false.
+ */
 int isDir(dString path) {
   Stat statBuf;
 
@@ -47,11 +84,25 @@ int isDir(dString path) {
   return 0;
 }
 
+/**
+ * Function: randInt
+ * ----------------------------
+ *   @brief generate a random int.
+ *
+ *   @return a random int.
+ */
 int randInt() {
   srand(time(0));
   return rand();
 }
 
+/**
+ * Function: printDebugMsg
+ * ----------------------------
+ *   @brief print debug message
+ *
+ *   @param msg message to be displayed.
+ */
 void printDebugMsg(dString msg) {
   if (superGlobal.isDebug == 1) {
     printf("%s\n", msg);
