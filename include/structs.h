30677675f69b38e5c00bd6e814869b55d50ce77d
Thalles Koester <thallesdella@gmail.com>
2019-12-09 22:27:59 -0300

    Formatted structs.h to ClangFormat

--- include/structs.h
+++ include/structs.h
@@ -8,40 +8,40 @@
 #include "dstring.h"
 
 typedef struct Option {
-    int status;
+  int status;
 
-    int (*verify)(dString str);
+  int (*verify)(dString str);
 } Option;
 
 typedef struct Flags {
-    unsigned int count;
-    unsigned int active;
-    Option *flags;
+  unsigned int count;
+  unsigned int active;
+  Option *flags;
 } Flags;
 
 typedef struct SearchTerm {
-    unsigned int count;
-    dStringVector terms;
+  unsigned int count;
+  dStringVector terms;
 } SearchTerm;
 
 typedef struct Target {
-    int isFile;
-    int isDir;
-    unsigned int hotLines;
-    unsigned int occurrences;
-    dString path;
+  int isFile;
+  int isDir;
+  unsigned int hotLines;
+  unsigned int occurrences;
+  dString path;
 } Target;
 
 typedef struct Targets {
-    unsigned int count;
-    unsigned int totalHotLines;
-    unsigned int totalOccurrences;
-    Target *targets;
+  unsigned int count;
+  unsigned int totalHotLines;
+  unsigned int totalOccurrences;
+  Target *targets;
 } Targets;
 
 typedef struct SuperGlobal {
-    //int (*isDebug)(void);
-    int isDebug;
+  // int (*isDebug)(void);
+  int isDebug;
 } SuperGlobal;
 
 SuperGlobal superGlobal;
